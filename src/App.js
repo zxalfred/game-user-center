f8a6cb455ac8e75b89c7164aca8f927340b9a6f2
zxalfred <zxsprout@foxmail.com>
2019-10-11 15:19:42 +0800

    init redux&router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import {
+  BrowserRouter as Router, Route, Link, Switch,
+} from 'react-router-dom'
+import store from '@/store'
+
+import Home from '@/pages/home'
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Route path="/home" component={Home}></Route>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 

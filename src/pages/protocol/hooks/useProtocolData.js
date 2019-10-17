c081fd9e85e62108ec7ee1b14558cb1490851f82
zxalfred <zxsprout@foxmail.com>
2019-10-17 15:51:55 +0800

    update

--- src/pages/protocol/hooks/useProtocolData.js
+++ src/pages/protocol/hooks/useProtocolData.js
@@ -1,25 +1,26 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { useLocation } from 'react-router-dom'
 import { useISelector } from '@/utils/hooks'
 import { getUrlQuery } from '@/utils'
 import { setLoading } from '@/store/actionCreators'
 import { actionCreators } from '../store'
 
 export default function useProtocolData() {
-  const protocolData = useISelector((state) => state.getIn(['protocol', 'protocolData']).toJS())
-  const type = getUrlQuery('type')
+  const protocolData = useISelector((state) => state.getIn(['protocol', 'protocolData'])).toJS()
   const dispatch = useDispatch()
-
+  const location = useLocation()
 
   useEffect(() => {
     const getProtocolData = async () => {
+      const type = getUrlQuery('type', location.search)
       const action = actionCreators.getProtocolData(type)
       dispatch(setLoading(true))
       await dispatch(action)
       dispatch(setLoading(false))
     }
     getProtocolData()
-  }, [])
+  }, [location])
 
   return protocolData
 }
--- src/pages/protocol/index.js
+++ src/pages/protocol/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 import useProtocolData from './hooks/useProtocolData'
 
 function Protocol() {
